0226074c4384f8a449f11db86401a4b63de66e63
LangusMcgee <77115641+LangusMcgee@users.noreply.github.com>
2025-11-28 02:34:14 +0000

    Added Transforms
    :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface:

--- RayTracer/Inc/Camera.h
+++ RayTracer/Inc/Camera.h
@@ -1,11 +1,18 @@
 #pragma once
 #include "Ray.h"
 
+class Transform {};
+
 class Camera
 {
 public:
 	Camera();
 	Ray createRay(glm::ivec2 _winPos);
+
+	void rotate(float _pitch, float _yaw, float _roll);
+
+	Transform transform;
+	//void translate
 private:	
 	glm::vec3 position;
 	glm::vec3 direction;
