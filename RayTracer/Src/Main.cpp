db2da0c32ebfcb565851b0e9cb020b2519f52ec5
LangusMcgee <77115641+LangusMcgee@users.noreply.github.com>
2025-12-01 05:48:27 +0000

    comments

--- RayTracer/Src/Main.cpp
+++ RayTracer/Src/Main.cpp
@@ -4,12 +4,18 @@
 #include "RayTracer.h"
 #include "Sphere.h"
 #include "Camera.h"
+#include <thread>
 //#include "helpful.h"
 
 
 int main(int argc, char* argv[])
 {
-	// Set window size
+	unsigned int n = std::thread::hardware_concurrency();
+	std::cout << n << " concurrent threads are supported.\n";
+
+
+
+	// Initialise window //////////////////////////////////////////
 	glm::ivec2 winSize(750, 750);
 
 	// This will handle rendering to screen
@@ -20,73 +26,61 @@ int main(int argc, char* argv[])
 	{
 		return -1;
 	}
+	//////////////////////////////////////////////////////////////
+
 
 	ray_tracer raytrcr;
+
+
+	// Setup Objects /////////////////////////////////////////////////////////////////////////////
 	std::shared_ptr <Sphere> sphere;
 	std::shared_ptr <Sphere> sphere2;
 	std::shared_ptr <Sphere> sphere3;
 	{
 
+		// create objects
 		sphere = std::make_shared<Sphere>(glm::vec3(0, 0, -5), glm::vec3(0, 0, 1), 1.0f);
 		sphere2 = std::make_shared<Sphere>(glm::vec3(-2, 0, -6), glm::vec3(1, 0, 0), 1.0f);
 		sphere3 = std::make_shared<Sphere>(glm::vec3(3, 0, -5), glm::vec3(1, 0, 0), 1.0f);
+
+		// add to raytracer object list
 		raytrcr.addSphere(sphere);
 		raytrcr.addSphere(sphere2);
 		raytrcr.addSphere(sphere3);
 	}
 
-	_myFramework.SetAllPixels(glm::vec3(0.1f, 0.1f, 1.0f));
+	/////////////////////////////////////////////////////////////////////////////////////////////
 
-	Camera cam;
-	cam.setResolution(winSize.x, winSize.y);
+	// set to pink because its better than nothing.
+	_myFramework.SetAllPixels(glm::vec3(1.0f, 0.0f, 1.0f));
+	_myFramework.RenderFrame();
+
+	Camera camera;
+	camera.setResolution(winSize.x, winSize.y);
 	glm::vec3 rotati(-1, 0, 1);
 
-	//cam.transform.rotation(rotati);
 
-	//print_vec3(sphere->position());
+	// RENDER LOOP /////////////////////////////////////////////////////////////////////////////
 	for (int i = 0; i < 20; i++)
 	{
-		//cam.transform.translate(rotati);
-		//cam.transform.rotate(rotati);
+		// Iterate through x and y axis using nested loop.
 		for (int i = 0; i < winSize.x; i++)
 		{
-			sphere->colour(glm::vec3(0, 1, 0));
 			for (int j = 0; j < winSize.y; j++)
 			{
-				glm::vec3 colour = raytrcr.trace_ray(cam.createRay(glm::ivec2(i, j)));
-				//std::cout << colour.x << " " << colour.y << " " << colour.z << "\n";
+				// Determine pixel colour
+				glm::vec3 colour = raytrcr.trace_ray(camera.createRay(glm::ivec2(i, j)));
+
+				// Draws shaded pixel
 				_myFramework.DrawPixel(glm::ivec2(i, j), colour);
 			}
 		}
 		_myFramework.RenderFrame();
+		// todo
+		// ensure window close works
 	}
+	/////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-	// Preparing a position to draw a pixel
-	glm::ivec2 pixelPosition = winSize / 2;
-
-	// Preparing a colour to draw
-	// Colours are RGB, each value ranges between 0 and 1
-	glm::vec3 pixelColour(1, 0, 0);
-
-
-	
-
-	// Sets all pixels the same colour
-
-
-	// Draws a single pixel
-	//_myFramework.DrawPixel(pixelPosition, pixelColour);
-
-
-
-
-
-	// Pushes the framebuffer to OpenGL and renders to screen
-	// Also contains an event loop that keeps the window going until it's closed
-	_myFramework.ShowAndHold();
 	return 0;
 
 
